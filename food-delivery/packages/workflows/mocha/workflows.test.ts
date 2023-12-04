118ccfe9ac6be0e09de740c83d0386a20cd21aa3
Jimmy Leger <jleger@lafourchette.com>
2023-12-04 23:11:24 +0100

    add test to take the order

--- food-delivery/packages/workflows/mocha/workflows.test.ts
+++ food-delivery/packages/workflows/mocha/workflows.test.ts
@@ -1,20 +1,29 @@
-import { WorkflowHandleWithFirstExecutionRunId } from '@temporalio/client'
+import { ApplicationFailure, WorkflowHandleWithFirstExecutionRunId } from '@temporalio/client'
 import { Runtime, DefaultLogger, Worker } from '@temporalio/worker'
-import { TestWorkflowEnvironment } from '@temporalio/testing'
+import { TestWorkflowEnvironment, workflowInterceptorModules } from '@temporalio/testing'
 import assert from 'assert'
 import { describe, it } from 'mocha'
+import sinon from 'sinon'
+import { v4 as uuid } from 'uuid'
 import * as activities from '../../activities'
-import { order } from '..'
+import { order, deliveredSignal, pickedUpSignal, getStatusQuery, OrderStatus } from '..'
 import { WorkflowCoverage } from '@temporalio/nyc-test-coverage'
+import { setTimeout } from 'timers/promises'
+import { Product, products } from 'common'
 
 const workflowCoverage = new WorkflowCoverage()
 
 describe('order workflow', async function () {
   let shutdown: () => Promise<void>
   let env: TestWorkflowEnvironment
-  let start: () => Promise<WorkflowHandleWithFirstExecutionRunId<typeof order>>
 
-  this.slow(15_000)
+  let execute: (productId: number) => ReturnType<typeof order>
+  let start: (productId: number) => Promise<WorkflowHandleWithFirstExecutionRunId<typeof order>>
+
+  let chargeCustomerStub: sinon.SinonStub
+  let sendPushNotificationStub: sinon.SinonStub
+
+  this.slow(10_000)
   this.timeout(20_000)
 
   before(async function () {
@@ -22,20 +31,24 @@ describe('order workflow', async function () {
     Runtime.install({ logger: new DefaultLogger('WARN') })
     env = await TestWorkflowEnvironment.createTimeSkipping()
 
-    let worker: Worker
-    try {
-      worker = await Worker.create(
-        workflowCoverage.augmentWorkerOptions({
-          connection: env.nativeConnection,
-          taskQueue: 'test-food-delivery',
-          workflowsPath: require.resolve('..'),
-          activities,
-        })
-      )
-    } catch (err) {
-      console.log(err)
-      process.exit(1)
-    }
+    chargeCustomerStub = sinon.stub()
+    sendPushNotificationStub = sinon.stub()
+
+    const worker = await Worker.create(
+      workflowCoverage.augmentWorkerOptions({
+        connection: env.nativeConnection,
+        taskQueue: 'test-food-delivery',
+        workflowsPath: require.resolve('..'),
+        //activities: mockActivities,
+        activities: {
+          ...activities,
+          ...{
+            chargeCustomer: chargeCustomerStub,
+            sendPushNotification: sendPushNotificationStub,
+          },
+        },
+      })
+    )
 
     const runPromise = worker.run()
     shutdown = async () => {
@@ -46,13 +59,23 @@ describe('order workflow', async function () {
   })
 
   beforeEach(() => {
-    start = () => {
+    start = (productId: number) => {
       return env.client.workflow.start(order, {
         taskQueue: 'test-food-delivery',
-        workflowExecutionTimeout: 10_000,
+        workflowExecutionTimeout: 30_000,
         // Use random ID because ID is meaningless for this test
-        workflowId: `test`,
-        args: [4], // Pass the expenseId property as a string
+        workflowId: `test-${uuid()}`,
+        args: [productId], // Pass the expenseId property as a string
+      })
+    }
+
+    execute = async (productId: number) => {
+      return env.client.workflow.execute(order, {
+        taskQueue: 'test-food-delivery',
+        workflowExecutionTimeout: 120_000,
+        // Use random ID because ID is meaningless for this test
+        workflowId: `test-${uuid()}`,
+        args: [productId], // Pass the expenseId property as a string
       })
     }
   })
@@ -65,15 +88,42 @@ describe('order workflow', async function () {
     workflowCoverage.mergeIntoGlobalCoverage()
   })
 
-  it('return a good answer', async () => {
-    // const result = await execute();
-    assert.equal(1, 1)
-    /*const handle = env.client.workflow.start(order, {
-            workflowId: 'test-order',
-            taskQueue: 'test-food-delivery',
-            args: [4],
-        });
+  afterEach(() => {
+    sinon.restore()
+  })
 
-        assert.ok(handle instanceof Promise);*/
+  it('rejects with a product not found', async () => {
+    const exec = async () => await execute(5)
+    await assert.rejects(exec, 'Product 5 not found')
+  })
+
+  it('sucessfully order a product', async () => {
+    const product = products[1]
+    chargeCustomerStub.withArgs(product).resolves()
+    sendPushNotificationStub.withArgs('🚗 Order picked up').onCall(0).resolves()
+    sendPushNotificationStub.withArgs('✅ Order delivered!').onCall(1).resolves()
+    sendPushNotificationStub
+      .withArgs(`✍️ Rate your meal. How was the ${product.name.toLowerCase()}?`)
+      .onCall(1)
+      .resolves()
+
+    let query: OrderStatus
+
+    const handle = await start(2)
+
+    // This generally takes less than one second, but allow up to 5 seconds for slow CI environments
+    await setTimeout(2000)
+
+    // Pick up the order
+    await handle.signal(pickedUpSignal)
+    query = await handle.query(getStatusQuery)
+    assert.strictEqual(query.state, 'Picked up')
+
+    // Deliver the order
+    await handle.signal(deliveredSignal)
+    query = await handle.query(getStatusQuery)
+    assert.strictEqual(query.state, 'Delivered')
+
+    await env.sleep('1 min')
   })
 })
